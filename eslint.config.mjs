8be786da4491cd5893842a1e364d9b289c4f02dd
Geon Lee <geon.lee@navercorp.com>
2025-04-19 00:56:00 +0900

    chore: 스타일 관련 설정 추가 및 Husky와 Commitlint 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
+import tailwindcss from 'eslint-plugin-tailwindcss'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -12,8 +13,14 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
   {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      tailwindcss: tailwindcss,
+    },
     rules: {
       'prettier/prettier': 'error',
+      'tailwindcss/no-custom-classname': 'warn',
+      'tailwindcss/classnames-order': 'warn',
     },
   },
 ]
